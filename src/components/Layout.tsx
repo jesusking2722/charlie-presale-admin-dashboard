843a2100c233a549c3a555698e639b3695b3325d
jesusking2722 <jesusking2722@gmail.com>
2025-07-15 19:41:57 +0900

    feat: add authenticate validation in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import { useAuth } from "./AuthProvider";
 
 const Layout = () => {
-  // if (!isAuthenticated) {
-  //   return <Navigate to="/login" replace />;
-  // }
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
 
   return (
     <div className="min-h-screen bg-background">
